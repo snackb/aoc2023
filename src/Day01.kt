9c47434b2d49236daffa838c2c9fc6d241dc9f6f
Jack Blanchard <jack.blanchard88@gmail.com>
2023-12-02 12:34:30 +1100

    Finish day1, setup day2

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,15 +1,56 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.map { str: String ->
+            str.first { chr -> chr.isDigit() }.toString() +  str.last { chr -> chr.isDigit() }.toString()
+        }.sumOf { str -> str.toInt() }
+    }
+
+    val numbers = listOf(
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine"
+    )
+
+    fun String.valueFirstDigit(): Char {
+        val firstActualDigit = this.indexOfFirst { chr -> chr.isDigit() }
+        val firstSpeltDigit = this.findAnyOf(numbers)
+        return if ( firstSpeltDigit == null || firstActualDigit < firstSpeltDigit.first && firstActualDigit != -1) {
+            this[firstActualDigit]
+        } else {
+            (numbers.indexOf(firstSpeltDigit.second) + 1).toString()[0]
+        }
+    }
+
+    fun String.valueLastDigit(): Char {
+        val lastActualDigit = this.indexOfLast { chr -> chr.isDigit() }
+        val lastSpeltDigitPair = this.findLastAnyOf(numbers)
+        return if ( lastSpeltDigitPair == null || lastActualDigit > lastSpeltDigitPair.first){
+            this[lastActualDigit]
+        } else {
+            (numbers.indexOf(lastSpeltDigitPair.second) + 1).toString()[0]
+        }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return input.sumOf{ line ->
+           val firstDigit = line.valueFirstDigit().toString()
+           val lastDigit = line.valueLastDigit().toString()
+            (firstDigit + lastDigit).toInt()
+        }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val test2Input = readInput("Day01_02_test")
+
+    check(part1(testInput) == 142)
+    println(part2(test2Input))
 
     val input = readInput("Day01")
     part1(input).println()
---
+++ src/Day02.kt
@@ -0,0 +1,16 @@
+fun main() {
+    val testResult = part1(readInput("Day02_test"))
+    testResult.println()
+    check(testResult == 0)
+
+    val realInput = readInput("Day02")
+    part1(realInput).println()
+    part2(realInput).println()
+}
+fun part1(input: List<String>): Int {
+    return 0
+}
+
+fun part2(input: List<String>): Int{
+    return 0
+}
